7842b262e0641840495a42585ad17da647e26f9a
sujin <rkdthd1234@naver.com>
2023-10-15 00:18:04 +0900

    Innitial Project

---
+++ Ahpuh/Ahpuh/AhpuhApp.swift
@@ -0,0 +1,17 @@
+//
+//  AhpuhApp.swift
+//  Ahpuh
+//
+//  Created by 강수진 on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct AhpuhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
